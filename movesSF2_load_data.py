a597713795c4e2ad2b602017b3322c126a543796
Sang-Young Jung <lycobs@gmail.com>
2022-10-25 00:27:43 +0900

    Added download pre-trained data to load_data script

--- movesSF2_load_data.py
+++ movesSF2_load_data.py
@@ -31,6 +31,7 @@ if __name__ == "__main__":
     parser.add_argument('--directory', type=str, default='./dataset')
     args = parser.parse_args()
     
+    current_dir = './'
     dataset_dir = args.directory
     try: os.makedirs(dataset_dir)
     except FileExistsError: pass
@@ -39,18 +40,22 @@ if __name__ == "__main__":
     urls.append('https://drive.google.com/uc?id=' + '1FUz3hGOIBvuA-6nq3dJyv-GX8KKaVSOk')
     urls.append('https://drive.google.com/uc?id=' + '1on4TI3zEtngz9y93dfHXgPgWG80EIvJS')
     urls.append('https://drive.google.com/uc?id=' + '15V1WtWts4IHsUGSbykIGXpRRAszqv3EM')
+    urls.append('https://drive.google.com/uc?id=' + '1rfJi0Wur9EryptYg8PeT89-Ma6J4-4iq')
     
     downloads = []
     downloads.append(dataset_dir + '/yolo_image_dataset.zip')
     downloads.append(dataset_dir + '/time_series_image_dataset.zip')
     downloads.append(dataset_dir + '/yolo_video.zip')
+    downloads.append(current_dir + '/movesSF2_pre_trained.hd5')
     
     outputs = []
     outputs.append(dataset_dir + '/images')
     outputs.append(dataset_dir + '/images_moves')
     outputs.append(dataset_dir + '/videos')
+    outputs.append(current_dir)
     
     for idx in range(len(urls)):
-        is_unzip = download(urls[idx], downloads[idx])
-        if is_unzip: unzip(downloads[idx], outputs[idx])
+        success = download(urls[idx], downloads[idx])
+        if downloads[idx].lower().endswith(('.zip')) and success:
+            unzip(downloads[idx], outputs[idx])
 
